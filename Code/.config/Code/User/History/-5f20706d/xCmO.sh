98711e018cf91fb82b2c9cda5f80f86b38016841
kicsirigo <kicsirigo0005@gmail.com>
2026-04-23 17:22:52 +0200

    removed Code

--- Code/.config/Code/User/History/-5f20706d/xCmO.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-get_brightness() {
-    brightnessctl -m | cut -d, -f4 | tr -d '%'
-}
-
-get_brightness
-while inotifywait -q -e modify /sys/class/backlight/*/brightness > /dev/null; do
-    get_brightness
-done
