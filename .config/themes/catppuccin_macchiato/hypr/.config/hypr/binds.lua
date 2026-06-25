b3561f69b615cae4663f274ca16a6c27c7a7dae0
mate <mate@example.com>
2026-06-25 09:16:41 +0200

    Fix aliases scope and binds references to support native Lua configurations

--- .config/themes/catppuccin_macchiato/hypr/.config/hypr/binds.lua
+++ .config/themes/catppuccin_macchiato/hypr/.config/hypr/binds.lua
@@ -2,12 +2,12 @@
 
 local mainMod = "SUPER"
 
-hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd("$terminal"))
+hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit"))
-hl.bind(mainMod .. " + E", hl.dsp.exec_cmd("$fileManager"))
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + Y", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + R", hl.dsp.exec_cmd("$menu"))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ mode = "maximized", action = "toggle" }))
 hl.bind(mainMod .. " + A", hl.dsp.window.fullscreen({ mode = "fullscreen", action = "toggle" }))
 hl.bind(mainMod .. " + SHIFT + P", hl.dsp.exec_cmd("kitty -T monitor-switcher -e hyprmode"))
